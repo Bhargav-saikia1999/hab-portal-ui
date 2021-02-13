4b80f1808f27eeeb73efdddc39fca7f80b0db3d0
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2021-02-14 03:34:28 +0530

    navbar implemented

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,19 @@
+const el = document.querySelector(".tabs");
+const listOfTabs = document.querySelectorAll(".tab a");
+
 document.addEventListener("DOMContentLoaded", (e) => {
-  const el = document.querySelector(".tabs");
   var instance = M.Tabs.init(el, {});
 });
+
+for (let i = 0; i < listOfTabs.length; i++) {
+  listOfTabs[i].addEventListener("click", (e) => {
+    const clickedTab = e.target.parentNode;
+    clickedTab.classList.add("tabStyle");
+
+    const tabs = e.target.parentNode.parentNode.children;
+    for (let tab of tabs) {
+      if (tab !== e.target.parentNode && tab.classList[0] !== "indicator")
+        tab.classList.remove("tabStyle");
+    }
+  });
+}
